8741d2803b38b0ed00149d16da54d3cc99a68c27
Breno Soares <francbreno@gmail.com>
2019-09-17 01:25:05 -0300

    Updated Spring boot version to 2.2.0

--- src/main/java/com/breno/projects/stockhandling/DemoApplication.java
+++ src/main/java/com/breno/projects/stockhandling/DemoApplication.java
@@ -1,4 +1,4 @@
-package com.example.stockhandling;
+package com.breno.projects.stockhandling;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
